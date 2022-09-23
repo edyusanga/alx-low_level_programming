56f860dbe411adb52139768f7bbec603a7a8a8a8
Edet Usanga <edyusanga@gmail.com>
2022-09-23 07:45:53 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 /**
- * *strncpy -> *strncpy for copying a string
+ * _strncpy -> *strncpy for copyimg a string
  * @dest: param1
  * @src: param2
  * @n: param3
  * Return: string
  */
 
-char *strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
 	return (dest);
